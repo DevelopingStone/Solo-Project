b66a815162185befbbbedb428a1988b2b036ea54
Knight <kangrack333@gmail.com>
2023-10-28 17:50:01 +0900

    refactor : AppConfig.java 역할,구현 구분

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -10,10 +10,18 @@ import hello.core.service.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private RateDiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
