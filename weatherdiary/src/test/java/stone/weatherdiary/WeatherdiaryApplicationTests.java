c6167c7770d0ef56d6e9f1b639af5096eace4c52
Stone <kangrack333@gmail.com>
2024-01-18 21:40:35 +0900

    feat : TDD 코드 추가

--- weatherdiary/src/test/java/stone/weatherdiary/WeatherdiaryApplicationTests.java
+++ weatherdiary/src/test/java/stone/weatherdiary/WeatherdiaryApplicationTests.java
@@ -1,13 +1,28 @@
 package stone.weatherdiary;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeatherdiaryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+    @Test
+    public void equalsTest() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void nullTest() {
+        assertNull(null);
+    }
+
+    @Test
+    void trueTest() {
+        Assertions.assertTrue(1 == 1);
+    }
 }
