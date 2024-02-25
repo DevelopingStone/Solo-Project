d8cd183e12fa97a1417e04441ebfc9ccaab21412
Stone <kangrack333@gmail.com>
2024-02-25 23:34:09 +0900

    feat : 메일인증 시스템 메소드 구현

--- src/main/java/com/example/ecommerce/config/FeignConfig.java
+++ src/main/java/com/example/ecommerce/config/FeignConfig.java
@@ -13,6 +13,7 @@ public class FeignConfig {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
         return new BasicAuthRequestInterceptor("api","28de5138e8d0eb1d3622839e5a682b94-408f32f3-6df803fb");
+
     }
 
 }
