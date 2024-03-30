f0fe2e0da908d88bf01df9b9f1e0a1f7de221ed8
Stone <kangrack333@gmail.com>
2024-03-30 20:17:50 +0900

    refactor : OauthService 불필요한 주석제거  - 주석제거 > log.debug 수정

--- src/main/java/com/example/weddinginvitation/member/service/OauthService.java
+++ src/main/java/com/example/weddinginvitation/member/service/OauthService.java
@@ -1,5 +1,6 @@
 package com.example.weddinginvitation.member.service;
 
+import com.example.weddinginvitation.member.controller.MemberController;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -11,26 +12,29 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OauthService {
 
+    private static final Logger logger = LoggerFactory.getLogger(OauthService.class);
+
     public String getAccessToken(String authorize_code) {
+        String reqURL = "https://kauth.kakao.com/oauth/token";
         String access_Token = "";
         String refresh_Token = "";
-        String reqURL = "https://kauth.kakao.com/oauth/token";
-
 
         try {
             URL url = new URL(reqURL);
 
+            logger.info("POST 요청을 위해 기본값이 false인 setDoOutput을 true로 변환");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            // POST 요청을 위해 기본값이 false인 setDoOutput을 true로
 
+            logger.info("POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송");
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
-            // POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
 
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
@@ -42,11 +46,9 @@ public class OauthService {
             bw.write(sb.toString());
             bw.flush();
 
-            // 결과 코드가 200이라면 성공
             int responseCode = conn.getResponseCode();
-//            System.out.println("responseCode : " + responseCode);
 
-            // 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            logger.info("// 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기");
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line = "";
             String result = "";
@@ -54,17 +56,13 @@ public class OauthService {
             while ((line = br.readLine()) != null) {
                 result += line;
             }
-//            System.out.println("response body : " + result);
 
-            // Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            logger.info("Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성");
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
 
             access_Token = element.getAsJsonObject().get("access_token").getAsString();
             refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
-
-//            System.out.println("access_token : " + access_Token);
-//            System.out.println("refresh_token : " + refresh_Token);
 
             br.close();
             bw.close();
@@ -75,8 +73,7 @@ public class OauthService {
     }
 
     public HashMap<String, Object> getUserInfo(String access_Token) {
-
-        // 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+        logger.info("요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언");
         HashMap<String, Object> userInfo = new HashMap<String, Object>();
         String reqURL = "https://kapi.kakao.com/v2/user/me";
         try {
@@ -84,12 +81,10 @@ public class OauthService {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
 
-            // 요청에 필요한 Header에 포함될 내용
+            logger.info("요청에 필요한 Header에 포함될 내용");
             conn.setRequestProperty("Authorization", "Bearer " + access_Token);
 
             int responseCode = conn.getResponseCode();
-//            System.out.println("responseCode : " + responseCode);
-
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
             String line = "";
@@ -98,7 +93,6 @@ public class OauthService {
             while ((line = br.readLine()) != null) {
                 result += line;
             }
-//            System.out.println("response body : " + result);
 
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
