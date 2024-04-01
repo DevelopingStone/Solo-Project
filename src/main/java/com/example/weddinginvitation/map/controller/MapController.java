55c92758d3b9f9f6f5d35bbc6eef531f0aad748b
Stone <kangrack333@gmail.com>
2024-04-01 22:18:40 +0900

    feat : 카카오맵 위치 공유 구현  - kakao_map.html 원하는 위치 설정  - MapController.java 컨트롤러를 통해 접근

---
+++ src/main/java/com/example/weddinginvitation/map/controller/MapController.java
@@ -0,0 +1,34 @@
+package com.example.weddinginvitation.map.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/map")
+public class MapController {
+
+    private final ResourceLoader resourceLoader;
+
+    @GetMapping("/location")
+    public ResponseEntity<String> loadFile() {
+        try {
+            Resource resource = resourceLoader.getResource("classpath:kakao_map.html");
+
+            // 파일을 문자열로 읽어오기
+            String fileContent = new String(resource.getInputStream().readAllBytes());
+            // 파일 내용 반환
+            return ResponseEntity.ok(fileContent);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error loading file");
+        }
+    }
+}
