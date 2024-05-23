fe4999d76025fb71ce173e1023233de2cf1c8423
Boss <kangrack333@gmail.com>
2024-05-24 05:24:40 +0900

    단체프로젝트 CI/CD 구성13

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
-echo "--------------서버시작--------------"
-
-cd /root/instagram-server
+echo "--------------- 서버 배포 시작 -----------------"
+cd /home/ubuntu/instagram-server
 sudo fuser -k -n tcp 8080 || true
 nohup java -jar project.jar > ./output.log 2>&1 &
-echo "--------------서버배포 끝--------------"
+echo "--------------- 서버 배포 끝 -----------------"
